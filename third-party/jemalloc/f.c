027411110ca20dd60551774c1d6d714dd2357dc7
Elliot Ronaghan <ronawho@gmail.com>
2017-10-05 16:43:49 -0700

    Simpler example code

--- third-party/jemalloc/f.c
+++ third-party/jemalloc/f.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <assert.h>
 #define JEMALLOC_NO_DEMANGLE
+
 #include "jemalloc/jemalloc.h"
 
 static extent_hooks_t *old_hooks;
@@ -14,55 +15,31 @@ static void* extent_alloc(extent_hooks_t *extent_hooks, void *new_addr, size_t s
 int main () {
   unsigned int arenas;
   size_t size = sizeof(unsigned int);
-  if (je_mallctl("arenas.narenas", &arenas, &size, NULL, 0)) {
-    printf("narenas failure\n");
-  }
-  printf("%d arenas\n", (int) arenas);
+  // get the number of arenas
+  assert(je_mallctl("arenas.narenas", &arenas, &size, NULL, 0) == 0);
+  printf("There are %d arenas\n", (int)arenas);
 
+  // grab a copy of the default extent_hooks
   size = sizeof(extent_hooks_t*);
-  if (je_mallctl("arena.0.extent_hooks", &old_hooks, &size, NULL, 0)) {
-    printf("extent get hooks failure\n");
-  }
-  
+  assert(je_mallctl("arena.0.extent_hooks", &old_hooks, &size, NULL, 0) == 0);
+ 
+  // setup "new" hooks, old hooks but alloc calls new shim
   extent_hooks_t * new_hooks = malloc(sizeof(extent_hooks_t));
   memcpy(new_hooks, old_hooks, sizeof(extent_hooks_t));
-  //new_hooks->alloc = extent_alloc;
+  new_hooks->alloc = extent_alloc;
 
+  // initialize all arenas with "thread.arena" call, then replace the extent
+  // hooks for all arenas, finally reset current "thread.arena" to 0
   unsigned arena;
-
   for (arena=0; arena<arenas; arena++) {
     char path[128];
     snprintf(path, sizeof(path), "arena.%u.extent_hooks", arena);
-    if (je_mallctl("thread.arena", NULL, NULL, &arena, sizeof(arena)) != 0) {
-      printf("could not change current thread's arena");
-    }
-    if (je_mallctl(path, NULL, NULL, &new_hooks, sizeof(extent_hooks_t*)) != 0) {
-      printf("extent set hooks failure\n");
-    }
+    assert(je_mallctl("thread.arena", NULL, NULL, &arena, sizeof(arena)) == 0);
+    assert(je_mallctl(path, NULL, NULL, &new_hooks, sizeof(extent_hooks_t*)) ==0);
   }
-
   arena = 0;
-  if (je_mallctl("thread.arena", NULL, NULL, &arena, sizeof(arena)) != 0) {
-      printf("could not change current thread's arena back to 0");
-  }
+  assert(je_mallctl("thread.arena", NULL, NULL, &arena, sizeof(arena)) == 0);
 
-
-//  if (je_mallctl("arena.1.extent_hooks", NULL, NULL, &new_hooks, sizeof(extent_hooks_t*))) {
-//    printf("extent set hooks failure\n");
-//  }
-
-  printf("%p\n", chpl_je_malloc(100000000));
-
-
-
-
-//  for(unsigned int i = 0; i < arenas; i++) {
-//      char buffer[25];
-//      
-//      snprintf(buffer, 25, "arena.%d.chunk_hooks", i);
-//
-//      size_t chunk_struct_size = sizeof(chunk_hooks_t);
-//
-//      je_mallctl(buffer, NULL, NULL, &mm_hooks, chunk_struct_size);
-//  }
+  // make an allocation that'll require a new extent
+  printf("%p\n", je_malloc(100000000));
 }
