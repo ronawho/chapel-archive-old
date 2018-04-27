0e35240725018f1384fc32e430639ece013b939a
Elliot Ronaghan <ronawho@gmail.com>
2018-04-27 16:29:56 -0700

    Revert "Fix licm bug involving references"
    This reverts commit 538386e6f73706c6513a631d3c66899a82fd7ca2.

--- compiler/optimizations/loopInvariantCodeMotion.cpp
+++ compiler/optimizations/loopInvariantCodeMotion.cpp
@@ -898,14 +898,6 @@ static void computeLoopInvariants(std::vector<SymExpr*>& loopInvariants,
         mightHaveBeenDeffedElseWhere = true;
       }
     }
-    if (symExpr->symbol()->isRef()) {
-        mightHaveBeenDeffedElseWhere = true;
-    }
-    for_set(Symbol, aliasSym, aliases[symExpr->symbol()]) {
-      if (aliasSym->isRef()) {
-        mightHaveBeenDeffedElseWhere = true;
-      }
-    }
     //if there were no defs of the symbol, it is invariant 
     if(actualDefs.count(symExpr) == 0 && !mightHaveBeenDeffedElseWhere) {
       loopInvariantOperands.insert(symExpr);
